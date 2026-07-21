f508ce7e2ca5ec7739a38eed7ef5aa7c1fd21221
celeste <aeolian_y@163.com>
2026-07-21 16:52:59 +0800

    v721.2

--- run.sh
+++ run.sh
@@ -1,9 +1,14 @@
+# 图像目标检测测试
 # python predict.py
 
+# 训练测试
 # python train.py
 
+# 视频目标检测测试
 # python video_od.py
 
+# 视频实例分割测试
 # python video_seg.py
 
-python instance_seg.py
+# 图像实例分割测试
+python ins_seg.py
